aa2a80e5de718f89dc0da76f8fb7f874b3b491ec
Vattsal Bhatt <vatsbhatt333@gmail.com>
2024-11-22 18:13:25 +0530

    cloudfront path added

--- app/products/page.js
+++ app/products/page.js
@@ -37,7 +37,7 @@ export default function Products() {
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const response = await fetch(`https://triquench-backend.vercel.app/api/product/all?category=${encodeURIComponent(activeCategory)}`);
+                const response = await fetch(`https://d1w2b5et10ojep.cloudfront.net/api/product/all?category=${encodeURIComponent(activeCategory)}`);
                 const data = await response.json();
                 setProducts(data || []); // Assuming the data is an array of products
             } catch (error) {
