024e49d46d3225333026111fc6ffa7329d0fa057
Renuka <socialmedia@tqipl.com>
2024-12-02 18:05:24 +0530

    Your commit message

--- components/homeComponents/videoSection.js
+++ components/homeComponents/videoSection.js
@@ -4,11 +4,16 @@ export default function VideoSection() {
   return (
     <>
       <section className="video-section">
-          <div className="video-wrapper">
-            <iframe width="100%" height="600" src="https://www.youtube.com/embed/c2jyH84SRS4?si=k8ZT5FkVn-KnpGfN"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-          </div>
+        <div className="video-wrapper">
+          <iframe
+            width="100%"
+            height="400"
+            src="https://www.youtube.com/embed/c2jyH84SRS4?si=k8ZT5FkVn-KnpGfN&autoplay=1&mute=1&vq=hd1080"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
       </section>
     </>
   );
-};
-
+}
