8cb640f5f49e64d1d3d1b9e5169b2ad645c39ac2
Renuka <socialmedia@tqipl.com>
2024-12-04 18:06:21 +0530

    section wise changes

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import BackToTop from './commonComponents/BackToTop';
 
 export default function Footer() {
   return (
@@ -138,12 +139,65 @@ export default function Footer() {
             </a>
           </div>
         </div>
-        <div className="footer-bottom d-flex align-center justify-sb">
+        <div className="footer-bottom d-flex align-center justify-center">
           <div className="left">
             © <span className="year">2024</span>, Triquench India PVT. LTD.
           </div>
+          <div className="right">
+           {/*  <ul className="d-flex align-center">
+              <li>
+                <Image
+                  src="/images/visa.svg"
+                  width={36}
+                  height={22}
+                  alt="visa"
+                />
+              </li>
+              <li>
+                <Image
+                  src="/images/diners.svg"
+                  width={36}
+                  height={22}
+                  alt="diners"
+                />
+              </li>
+              <li>
+                <Image
+                  src="/images/master-card.svg"
+                  width={36}
+                  height={22}
+                  alt="mastercard"
+                />
+              </li>
+              <li>
+                <Image
+                  src="/images/american-express.svg"
+                  width={36}
+                  height={22}
+                  alt="americanExpress"
+                />
+              </li>
+              <li>
+                <Image
+                  src="/images/discover.svg"
+                  width={36}
+                  height={22}
+                  alt="discover"
+                />
+              </li>
+              <li>
+                <Image
+                  src="/images/paypal.svg"
+                  width={36}
+                  height={22}
+                  alt="paypal"
+                />
+              </li>
+            </ul>*/}
+          </div>
         </div>
       </div>
+      <BackToTop />
     </footer>
   );
 }
