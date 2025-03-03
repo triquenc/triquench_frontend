50d9661eb54216b8d76490bdaec800d110f7923e
triquenc <151922322+triquenc@users.noreply.github.com>
2025-03-03 18:46:52 +0530

    Update app.js
    no changes

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-import { useEffect } from "react";
-import { initGA, logPageView } from "./utils/gtag";
 
-function App() {
-    useEffect(() => {
-        initGA();
-        logPageView();
-    }, []);
-
-    return <div>My React App with Google Analytics</div>;
-}
-
-export default App;
