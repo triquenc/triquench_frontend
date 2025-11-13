65ff40bff63f2fe7409d7d24db501f3cc3a0a5f1
Vishwash Meniya <meniyavishwash23@gmail.com>
2025-11-13 12:34:40 +0530

    fix code

--- components/homeComponents/formSection.js
+++ components/homeComponents/formSection.js
@@ -24,7 +24,7 @@ const FormSection = () => {
 
   const handleSubmit = async (values, { resetForm }) => {
     try {
-      const response = await fetch("https://triquench.ap-south-1.elasticbeanstalk.com/api/form/submit", {
+      const response = await fetch("http://localhost:5000/api/form/submit", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
